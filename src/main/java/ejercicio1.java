15222311ac4095fa359593d9aac7ba8f7905eb60
Johancastano88 <johans19921@gmail.com>
2022-09-13 08:08:58 -0500

    proyecto mi primer commit

---
+++ src/main/java/ejercicio1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fabiola
+ */
+public class ejercicio1 {
+    
+    
+    public static void main(String[] args) { 
+    System.out.println("hola estoy escribiendo codigo pata subirlo a Git");
+    }
+    
+}
